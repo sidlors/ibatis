f3f7da20a8487fd781bca1339deef2ccbf81ce86
jhernandezn <sidlors@github.com>
2015-08-14 21:43:58 -0500

    first load to ibatis project

---
+++ src/main/java/es/indra/hls/indrasid/business/mec/dao/services/Employee.java
@@ -0,0 +1,18 @@
+package es.indra.hls.indrasid.business.mec.dao.services;
+
+public class Employee {
+	private int id;
+	private String first_name;
+	private String last_name;
+	private int salary;
+
+	/* Define constructors for the Employee class. */
+	public Employee() {
+	}
+
+	public Employee(String fname, String lname, int salary) {
+		this.first_name = fname;
+		this.last_name = lname;
+		this.salary = salary;
+	}
+} /* End of Employee */
